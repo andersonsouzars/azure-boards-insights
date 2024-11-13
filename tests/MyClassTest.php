78ac38d5d1834c134561e6e047befebc492d991b
Anderson Souza <anderson@datamex.com>
2024-11-12 23:22:04 -0300

    chore: Adiciona xdebug

---
+++ tests/MyClassTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\MyClass;
+
+class MyClassTest extends TestCase
+{
+    public function testAdd()
+    {
+        $obj = new MyClass();
+        $this->assertEquals(4, $obj->add(2, 2));
+    }
+}
